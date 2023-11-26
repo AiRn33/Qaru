3c736dc4a3ea6367b6b3ddf9d859c1b4ac666512
kim <tkdqhr6777@naver.com>
2023-11-26 16:53:15 +0900

    11-26
    공통 js 생성 시작

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -58,3 +58,7 @@ function email_check( email ) {
     var regex=/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
     return (email != '' && email != 'undefined' && regex.test(email));
 }
+
+function number_check(number){
+
+}
